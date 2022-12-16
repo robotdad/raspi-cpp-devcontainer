8c45c864b474588254bc77c3395c8ac286002a38
Marc Goodner <mgoodner@microsoft.com>
2022-12-15 17:37:06 -0800

    Devcontainer, hello world, and launch configs for VS/VSCode

---
+++ main.cpp
@@ -0,0 +1,22 @@
+﻿// main.cpp : Defines the entry point for the application.
+//
+
+#include "main.h"
+#include <sys/utsname.h>
+#include <sys/unistd.h>
+
+using namespace std;
+
+int main()
+{
+	struct utsname buffer;
+	uname(&buffer);
+
+	while (1)
+	{
+		std::cout << "Hello from " << buffer.nodename << std::endl;
+		sleep(2);
+	}
+
+	return 0;
+}
---
+++ main.h
@@ -0,0 +1,6 @@
+﻿// main.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
